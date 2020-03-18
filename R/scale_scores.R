c903ea9b68a78dfbfe519a6522b557c667062058
Jimmy Rigby <jrigby@pros.com>
2020-03-18 13:57:22 -0500

    updated so drop arguments are optional

--- R/scale_scores.R
+++ R/scale_scores.R
@@ -29,42 +29,50 @@
 #' cleaned<-scale_scores(data = personality, A1, E1, E2, O2, O5, C4, C5)
 
 scale_scores<-function(data, ..., completed_thresh = .75, scales_only = FALSE, scale_regex="^[A-Za-z[:punct:]]*", item_regex="[0-9]+$|[0-9]+r$|[0-9]+_r"){
-
   dropquo <- dplyr::enquos(...)
 
+
   scaled <- data %>%
-    dplyr::mutate(unique_row_id = 1:nrow(data))%>%
-    dplyr::select_if(function(x){is.numeric(x)|is.integer(x)})%>%
-    dplyr::select(-c(!!!dropquo))%>%
-    tidyr::gather(key = key, value = value, -unique_row_id)%>%
-    dplyr::filter(stringr::str_detect(key, item_regex))%>%
-    dplyr::mutate(scale = stringr::str_extract(key,  scale_regex))%>%
-    dplyr::mutate(scale = as.factor(scale))%>%
-    dplyr::group_by(unique_row_id, scale) %>%
-    dplyr::summarize(scale_score = mean(value, na.rm = TRUE),
-                     na_prop = sum(is.na(value))/n())%>%
-    dplyr::mutate(scale_score = dplyr::if_else((1-completed_thresh)>na_prop,
-                                 scale_score,
-                                 as.numeric(NA)))%>%
-    dplyr::select(-na_prop)%>%
-    dplyr::filter(!is.na(scale))
+    dplyr::mutate(.unique_row_id = 1:nrow(data))%>% # adds a row id (useful for maintaining observations later on)
+    dplyr::select_if(function(x){is.numeric(x)|is.integer(x)}) # retains observations only if they are numeric to create scale scores
 
-  message("Scale scores created for ", dplyr::n_distinct(scaled$scale), " scales.\n",
-          "Scale Names:\n",
-          paste(unique(scaled$scale), collapse = " "))
-
+  if(length(dropquo>1)){
+   scaled<- scaled%>%
+     dplyr::select(-c(!!!dropquo)) # drops the specified columns if any are provided through dots
+  }
   scaled<-scaled%>%
-    tidyr::spread(key = scale, value = scale_score)%>%
-    dplyr::arrange(unique_row_id)%>%
+    tidyr::gather(key = key, value = value, -.unique_row_id)%>% # converts to long format. rows are now associated with person-record
+    dplyr::filter(stringr::str_detect(key, item_regex))%>% # retains only observations that match regular expression
+    dplyr::mutate(.scale = stringr::str_extract(key,  scale_regex))%>% # extracts the scale title
+    dplyr::mutate(.scale = as.factor(.scale))%>% # converts to factor for grouping
+    dplyr::group_by(.unique_row_id, .scale) %>% # groups by person scale
+    dplyr::summarize(.scale_score = mean(value, na.rm = TRUE), # calculates average of completed data
+                     na_prop = sum(is.na(value))/n())%>% # calculates missing proportion
+    dplyr::mutate(.scale_score = dplyr::if_else((1-completed_thresh)>na_prop, # if missing proportion is greater than threshold returns na
+                                 .scale_score,
+                                 as.numeric(NA)))%>%
+    dplyr::select(-na_prop)%>% # removes missing proportion
+    dplyr::filter(!is.na(.scale))
+
+  # prints verbose message
+  message("Scale scores created for ", dplyr::n_distinct(scaled$.scale), " scales.\n",
+          "Scale Names:\n",
+          paste(unique(scaled$.scale), collapse = " "))
+
+  # spreads so that rows are associted with original values and arranges so they are in teh same order
+  scaled<-scaled%>%
+    tidyr::spread(key = .scale, value = .scale_score)%>%
+    dplyr::arrange(.unique_row_id)%>%
     dplyr::ungroup()
 
+  # formats appropriately based on user arguments
   if(!scales_only){
     scaled<-scaled%>%
       dplyr::bind_cols(data)%>%
-      dplyr::select(colnames(data), dplyr::everything(), -unique_row_id)
+      dplyr::select(colnames(data), dplyr::everything(), -.unique_row_id)
   }else{
     scaled<-scaled%>%
-      splyr::select(-unique_row_id)
+      splyr::select(-.unique_row_id)
   }
 
     scaled
